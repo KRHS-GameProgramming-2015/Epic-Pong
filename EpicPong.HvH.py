ed8379ccf3115871d83fe4f2e00376c40830a48d
Aaron Le <Aaron.Le@kearsarge.org>
2016-02-09 12:33:03 -0500

    mode fixes, reset

--- EpicPong.HvH.py
+++ EpicPong.HvH.py
@@ -3,7 +3,6 @@ from PlayerPaddle import *
 from Ball import *
 from Score import *
 from Menu import *
-from Button import *
 pygame.init()
 
 clock = pygame.time.Clock()
@@ -24,14 +23,10 @@ while True:
             if event.type == pygame.QUIT: 
                 sys.exit()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_a:
+                if event.key == pygame.K_1:
                     mode = "pvp"
-                if event.key == pygame.K_b:
+                if event.key == pygame.K_2:
                     mode = "practice"
-                if event.key == pygame.K_RETURN:
-                    mode = ["player 1 win",
-                            "player 2 win",
-                            "practice lose"]
                     
         bg = pygame.image.load("Pics/menubackground.png")
         bgrect = bg.get_rect(center = [width/2,height/2])
@@ -51,7 +46,7 @@ while True:
     scoreP1 = Score([300, 350])
     scoreP2 = Score([600, 350])
 
-    endScore = 15
+    endScore = 1
     lastScore = random.randint(1,2)
     beginningScore = 0    
     while mode == "pvp":
@@ -250,10 +245,7 @@ while True:
                 sys.exit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN:
-                    scoreP1.reset()
-                    scoreP2.reset()
-                    player.reset()
-                    player2.reset()
+                    mode = "start"
                     
         playAgain = pygame.image.load("Pics/Player/playagain.png")
         playAgainRect = playAgain.get_rect(center = [width/2,3*height/4])
